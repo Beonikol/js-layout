ae0349141a18938469699f9435adf8ec8ed85002
Beonikol <alexratya@gmail.com>
2023-09-28 19:01:44 +0300

    js-layout

--- src/route/index.js
+++ src/route/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // Підключіть файли роутів
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
+const page2 = require('./page2')
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/page2', page2)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
